fc986ed20ed53ba8a8d311ca744504302a3f0319
Thejaswin S <52855622+thejaswin123@users.noreply.github.com>
2023-03-02 14:22:22 +0530

    Update app.js

--- app.js
+++ app.js
@@ -133,6 +133,6 @@ app.post("/delete",function(req,res){
 // });
 
 
-app.listen(process.env.port||3000,function(){
+app.listen(process.env.PORT||3000,function(){
     console.log("server is running on port 3000")
 });
